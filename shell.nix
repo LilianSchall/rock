2791d17b035f7ef2aff28b24f0e523941ea5b9bc
Lilian Schall <lilian.schall@epita.fr>
2025-06-05 00:06:19 +0200

    feat: added shell.nix

---
+++ shell.nix
@@ -0,0 +1,58 @@
+{pkgs ? import <nixpkgs>{}}:
+pkgs.mkShell rec {
+	name = "Test Env";
+	nativeBuildInputs = [];
+
+	buildInputs = with pkgs;[
+		libxkbcommon
+		libGL
+
+		SDL2
+		SDL2_ttf
+
+		boost		
+		cmake
+		catch2
+		glm
+		gcc
+
+		pkg-config
+
+		xorg.libX11
+		xorg.libXrandr
+		xorg.libXcursor
+		xorg.libXi
+		xorg.libXxf86vm
+
+		glfw3
+		mesa
+		glslang
+		renderdoc
+		spirv-tools
+		vulkan-volk
+		vulkan-tools
+		vulkan-loader
+		vulkan-headers
+		vulkan-validation-layers
+		vulkan-tools-lunarg
+		vulkan-extension-layer
+	];
+
+	packages = [];
+
+	shellHook = with pkgs; ''
+	echo "Welcome to my Vulkan Shell"
+	echo "vulkan loader: ${vulkan-loader}"
+	echo "vulkan headers: $vulkan-headers}"
+	echo "validation layer: ${vulkan-validation-layers}"
+	echo "tools: ${vulkan-tools}"
+	echo "tools-lunarg: ${vulkan-tools-lunarg}"
+	echo "extension-layer: ${vulkan-extension-layer}"
+	'';
+
+	LD_LIBRARY_PATH = "${pkgs.lib.makeLibraryPath buildInputs}";
+	VK_LAYER_PATH = "${pkgs.vulkan-validation-layers}/share/vulkan/explicit_layer.d";
+	VULKAN_SDK = "${pkgs.vulkan-validation-layers}/share/vulkan/explicit_layer.d";
+	XDG_DATA_DIRS = builtins.getEnv "XDG_DATA_DIRS";
+	XDG_RUNTIME_DIR = "/run/user/1000";
+}
