87dde924d0960762f4c79331338f0f64a6dfabae
Lilian Schall <lilian.schall@epita.fr>
2025-06-15 23:00:11 +0200

    feat: added frame buffer creation

--- main.cc
+++ main.cc
@@ -110,6 +110,7 @@ class HelloTriangleApplication
     VkRenderPass renderPass;
     VkPipelineLayout pipelineLayout;
     VkPipeline graphicsPipeline;
+    std::vector<VkFramebuffer> swapChainFramebuffers;
 
     const std::vector<const char *> validationLayers = {
         "VK_LAYER_KHRONOS_validation"
@@ -967,6 +968,33 @@ class HelloTriangleApplication
         }
     }
 
+    void createFramebuffers()
+    {
+        swapChainFramebuffers.resize(swapChainImageViews.size());
+        for (size_t i = 0; i < swapChainImageViews.size(); i++)
+        {
+            VkImageView attachments[] = { swapChainImageViews[i] };
+
+            VkFramebufferCreateInfo framebufferInfo{};
+            framebufferInfo.sType = VK_STRUCTURE_TYPE_FRAMEBUFFER_CREATE_INFO;
+            framebufferInfo.renderPass = renderPass;
+            framebufferInfo.attachmentCount = 1;
+            framebufferInfo.pAttachments = attachments;
+            framebufferInfo.width = swapChainExtent.width;
+            framebufferInfo.height = swapChainExtent.height;
+            framebufferInfo.layers = 1;
+
+            if (vkCreateFramebuffer(
+                    device,
+                    &framebufferInfo,
+                    nullptr,
+                    &swapChainFramebuffers[i]) != VK_SUCCESS)
+            {
+                throw std::runtime_error("failed to create framebuffer!");
+            }
+        }
+    }
+
     void initVulkan()
     {
         createInstance();
@@ -990,6 +1018,10 @@ class HelloTriangleApplication
 
     void cleanup()
     {
+        for (auto framebuffer : swapChainFramebuffers)
+        {
+            vkDestroyFramebuffer(device, framebuffer, nullptr);
+        }
         vkDestroyPipeline(device, graphicsPipeline, nullptr);
         vkDestroyPipelineLayout(device, pipelineLayout, nullptr);
         vkDestroyRenderPass(device, renderPass, nullptr);
